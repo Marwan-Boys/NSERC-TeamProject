7187ee202473c03b89dfec2bf7f04c6687aa411d
Nish <Nepal.Nishchal@NSERC-CRSNG.GC.CA>
2020-02-20 15:46:31 -0500

    Successfully launched React

--- website/src/App.js
+++ website/src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
     render() {
-
+        return (
+            <BrowserRouter>
+                <h1>
+                    Hello
+                </h1>
+            </BrowserRouter>
+        );
     }
 
 }
