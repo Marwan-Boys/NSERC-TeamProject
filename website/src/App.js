01424af007dd95df822827b505059f0c8bbddd82
Nish <Nepal.Nishchal@NSERC-CRSNG.GC.CA>
2020-02-21 08:01:04 -0500

    Revert "Added the Navigation Bar"
    This reverts commit 9932594e54be015643c9ae36dc12511cbecafc45.

--- website/src/App.js
+++ website/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import Navbar from "./Components/layout/NavigationBar";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <div className={"App"}>
-                    <Navbar></Navbar>
-                </div>
+                <h1>
+                    Hello
+                </h1>
             </BrowserRouter>
         );
     }
+
 }
 
 export default App;
