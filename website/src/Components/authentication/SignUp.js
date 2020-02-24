f4c851253bfee9f6f7f5af192786b771b0c70f40
nnepa066 <43126840+nnepa066@users.noreply.github.com>
2020-02-24 08:37:45 -0500

    Added visual text to show sign up

--- website/src/Components/authentication/SignUp.js
+++ website/src/Components/authentication/SignUp.js
@@ -20,7 +20,7 @@ class SignUp extends Component {
         return (
             <div className={"container"}>
                 <form onSubmit={this.handleSubmit} className={"white"}>
-                    <h5 className={"grey-text text-darken-3"}>Sign In</h5>
+                    <h5 className={"grey-text text-darken-3"}>Sign Up</h5>
                     <div className={"input-field"}>
                         <label htmlfor={"email"}>Email</label>
                         <input type={"email"} id={"email"} onChange={this.handleChange}/>
@@ -38,7 +38,7 @@ class SignUp extends Component {
                         <input type={"text"} id={"lastname"} onChange={this.handleChange}/>
                     </div>
                     <div className={"input-field"}>
-                        <button className={"btn pink lighten-1 z-depth0"}>Login</button>
+                        <button className={"btn pink lighten-1 z-depth0"}>Sign Up</button>
                     </div>
                 </form>
             </div>
