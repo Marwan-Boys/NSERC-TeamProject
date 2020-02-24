3a0f43d94e9734fc87e391a70a3f00849c6939ce
nnepa066 <43126840+nnepa066@users.noreply.github.com>
2020-02-24 08:29:39 -0500

    Added the signup code

---
+++ website/src/Components/authentication/SignUp.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+
+class SignOut extends Component {
+    state = {
+        email: '',
+        password: '',
+        firstname: '',
+        lastname: = ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div className={"container"}>
+                <form onSubmit={this.handleSubmit} className={"white"}>
+                    <h5 className={"grey-text text-darken-3"}>Sign In</h5>
+                    <div className={"input-field"}>
+                        <label htmlfor={"email"}>Email</label>
+                        <input type={"email"} id={"email"} onChange={this.handleChange}/>
+                    </div>
+                    <div className={"input-field"}>
+                        <label htmlFor={"password"}>Password</label>
+                        <input type={"password"} id={"password"} onChange={this.handleChange}/>
+                    </div>
+                    <div className={"input-field"}>
+                        <label htmlfor={"firstname"}>First Name</label>
+                        <input type={"text"} id={"firstname"} onChange={this.handleChange}/>
+                    </div>
+                    <div className={"input-field"}>
+                        <label htmlfor={"lastname"}>Last Name</label>
+                        <input type={"text"} id={"lastname"} onChange={this.handleChange}/>
+                    </div>
+                    <div className={"input-field"}>
+                        <button className={"btn pink lighten-1 z-depth0"}>Login</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default SignOut;
