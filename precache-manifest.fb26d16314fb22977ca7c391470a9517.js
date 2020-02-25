d42a42d8e7d27fc54052010d2093bdd8312c2146
nnepa066 <nishchalnepal12@yahoo.com>
2020-02-25 12:37:38 -0500

    Updates

---
+++ precache-manifest.fb26d16314fb22977ca7c391470a9517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6df121d69926e36a5ba93bc2f79512b",
+    "url": "/NSERC-TeamProject/index.html"
+  },
+  {
+    "revision": "e0c25bf02d066b46e979",
+    "url": "/NSERC-TeamProject/static/js/2.d29f3cfa.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/NSERC-TeamProject/static/js/2.d29f3cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb46a31053165489a3a4",
+    "url": "/NSERC-TeamProject/static/js/main.4d0f5053.chunk.js"
+  },
+  {
+    "revision": "460a4eededb0dff4f170",
+    "url": "/NSERC-TeamProject/static/js/runtime-main.48558775.js"
+  }
+]);
